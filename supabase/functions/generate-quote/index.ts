126963154b4fc863dd825df78350f18c74e65e29
momentoidoneo <momentoidoneo@gmail.com>
2026-07-30 15:56:23 +0200

    chore: expose quote catalog version

--- supabase/functions/generate-quote/index.ts
+++ supabase/functions/generate-quote/index.ts
@@ -22,6 +22,7 @@ const corsHeaders = {
   "Access-Control-Allow-Headers":
     "authorization, x-client-info, apikey, content-type",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "X-Quote-Catalog-Version": "2026-07-30",
 };
 
 interface QuoteRequest {
